eedb7297077e0800ce4c2148fd6db44d6a32ce96
loadthecode0 <artyaastha@gmail.com>
2025-09-05 22:26:52 +0530

    langs fixed

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 python3 main.py --dataset "flores_plus" \
-    --languages en es fr \
+    --languages en de fr it pt hi es th bg ru tr vi ja ko zh\
     --model_path "/home/models/meta-llama_Llama-3.2-1B" \
     --model_name "Llama-3.2-1B" \
     --sae_model "/home/models/sae-Llama-3.2-1B-131k" \
     --text_field "text" \
     --batch_size 16 \
     --split "devtest" \
-    --layers "layers.2.mlp"
+    --layers "layers.0.mlp"
